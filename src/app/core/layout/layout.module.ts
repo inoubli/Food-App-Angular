b1c25b044133f8f2554c5f7a30803daa343474e3
Amine <amine.rhouma@metr.systems>
2020-05-25 19:47:32 +0200

    Create layout module

---
+++ src/app/core/layout/layout.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MenuComponent } from './menu/menu.component';
+import { LayoutComponent } from './layout.component';
+
+
+
+@NgModule({
+  declarations: [MenuComponent, LayoutComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class LayoutModule { }
