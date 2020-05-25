939a9046595b01dc42cb7dfbc0e705180afe58b6
Amine <amine.rhouma@metr.systems>
2020-05-25 20:00:07 +0200

    Update home module

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home.component';
+import { FoodComponent } from './food/food.component';
+import { CategoryComponent } from './category/category.component';
+import { MealComponent } from './meal/meal.component';
 
 
 
 @NgModule({
-  declarations: [HomeComponent],
+  declarations: [HomeComponent, FoodComponent, CategoryComponent, MealComponent],
   imports: [
     CommonModule
   ]
