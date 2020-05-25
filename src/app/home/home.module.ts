2ff07afe7626f09c0aea321738afe7cbe8b97df2
Amine <amine.rhouma@metr.systems>
2020-05-25 20:14:39 +0200

    Create home router

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,16 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
 import { HomeComponent } from './home.component';
 import { FoodComponent } from './food/food.component';
 import { CategoryComponent } from './category/category.component';
 import { MealComponent } from './meal/meal.component';
-
-
+import { HomeRoutingModule } from './home-routing.module';
 
 @NgModule({
-  declarations: [HomeComponent, FoodComponent, CategoryComponent, MealComponent],
-  imports: [
-    CommonModule
-  ]
+  declarations: [
+    HomeComponent,
+    FoodComponent,
+    CategoryComponent,
+    MealComponent,
+  ],
+  imports: [CommonModule, HomeRoutingModule],
 })
-export class HomeModule { }
+export class HomeModule {}
