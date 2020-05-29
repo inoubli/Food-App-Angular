208cdc0a5f1b21627afc869cfb9dd310932f4b11
Inoubli Aymen <aymen.inoubli@esprit.tn>
2020-05-30 00:35:50 +0200

    Added food form

--- src/app/home/food/food.component.ts
+++ src/app/home/food/food.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { FoodService } from 'src/app/core/services';
 
 @Component({
   selector: 'app-food',
@@ -6,10 +8,37 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./food.component.css']
 })
 export class FoodComponent implements OnInit {
+  public foodForm: FormGroup;
 
-  constructor() { }
+  get name(){
+    return this.foodForm.get('name');
+  }
+
+  constructor(private foodService: FoodService) { }
 
   ngOnInit(): void {
+    this.buildForm();
+  }
+
+  public buildForm(){
+    this.foodForm = new FormGroup({
+      name: new FormControl('', Validators.required),
+      legend: new FormControl(),
+      selectedScore: new FormControl(),
+      deselectedScore: new FormControl(),
+      description: new FormControl(),
+      imageUrl: new FormControl(),
+      categoryId: new FormControl()
+    });
+  }
+
+  /**
+   * Save new food
+   */
+  public submit(){
+    this.foodService.create(this.foodForm.value).subscribe((data) => {
+      alert(`Food ${data.name} successfully created`);
+    });
   }
 
 }
