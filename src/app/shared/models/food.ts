a11edacb2d37dee75a2bf5a9dc66962cbfa6b0cf
Inoubli Aymen <aymen.inoubli@esprit.tn>
2020-05-30 00:32:18 +0200

    Added Food Interface

---
+++ src/app/shared/models/food.ts
@@ -0,0 +1,11 @@
+export interface Food {
+    id: number;
+    name: string;
+    description?: string;
+    legend: string;
+    created_at: string;
+    updated_at: string;
+    selectedScore: number;
+    deselectedScore: number;
+  }
+  
