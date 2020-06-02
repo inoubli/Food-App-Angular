baab029bb52b19e36e7c644414c588bf0910b123
Inoubli Aymen <aymen.inoubli@esprit.tn>
2020-06-02 05:30:33 +0200

    Added options param for api.service

--- src/app/core/services/api.service.ts
+++ src/app/core/services/api.service.ts
@@ -19,30 +19,30 @@ export class ApiService {
   get(path: string, params: HttpParams = new HttpParams()): Observable<any> {
     return this.http
       .get(`${environment.apiUrl}${path}`, { params })
-      .pipe(catchError(this.formatErrors));
+      // .pipe(catchError(this.formatErrors));
   }
 
   put(path: string, body: Object = {}): Observable<any> {
     return this.http
       .put(`${environment.apiUrl}${path}`, body)
-      .pipe(catchError(this.formatErrors));
+      // .pipe(catchError(this.formatErrors));
   }
 
   patch(path: string, body: Object = {}): Observable<any> {
     return this.http
       .patch(`${environment.apiUrl}${path}`, body)
-      .pipe(catchError(this.formatErrors));
+      // .pipe(catchError(this.formatErrors));
   }
 
-  post(path: string, body: Object = {}): Observable<any> {
+  post(path: string, body: Object = [{}], options:{}): Observable<any> {
     return this.http
-      .post(`${environment.apiUrl}${path}`, body)
-      .pipe(catchError(this.formatErrors));
+      .post(`${environment.apiUrl}${path}`, body, options)
+      // .pipe(catchError(this.formatErrors));
   }
 
   delete(path): Observable<any> {
     return this.http
       .delete(`${environment.apiUrl}${path}`)
-      .pipe(catchError(this.formatErrors));
+      // .pipe(catchError(this.formatErrors));
   }
 }
