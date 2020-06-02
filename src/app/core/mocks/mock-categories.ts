50b5c47feb2f81e5ca13ecaaa1a58ef69b0b129e
Inoubli Aymen <aymen.inoubli@esprit.tn>
2020-06-02 05:27:18 +0200

    Mock For Categories

---
+++ src/app/core/mocks/mock-categories.ts
@@ -0,0 +1,22 @@
+import { Category } from 'src/app/shared/models';
+
+export const CATEGORIES: Category[] = [
+    {
+        id: 1,
+        name: "Drinks",
+        description: "All alcoholic drinks",
+        legend: "alcohol",
+        created_at: "2020-05-23T23:39:31+00:00",
+        updated_at: "2020-05-23T23:39:31+00:00",
+        meals:[]
+    },
+    {
+        id: 2,
+        name: "Vegetable",
+        description: "All vegetable's diversities",
+        legend: "vegetable",
+        created_at: "2020-05-23T23:39:31+00:00",
+        updated_at: "2020-05-23T23:39:31+00:00",
+        meals:[]
+    }
+];
